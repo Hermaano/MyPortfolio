973047543ca80248c8ae494b0ef7761aeec434e3
MOHAMED ABDIHAKIN MUHUMED <156492655+Hermaano@users.noreply.github.com>
2024-03-04 19:29:34 +0800

    last commit

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -4,6 +4,7 @@ import { IoBookOutline } from "react-icons/io5";
 function Education() {
   return (
     <div className="educationSection mt-[170px] mb-10 text-black">
+
       <div className="container mx-auto px-2 bg-white py-2 rounded-md ">
         <h1 className="text-center text-3xl font-bold w-full border-b-2 uppercase border-[blue]">
           EDUCATION
@@ -30,12 +31,7 @@ function Education() {
           </div>
         </div>
 
-        <div className="flex flex-col justify-center items-center pl-10 my-2 ">
-          <h1 className="text-2xl font-bold ">Additional Courses </h1>
-          <p> &#x2022; Human-Computer Interaction</p>
-          <p> &#x2022; Software Quality & Measurements</p>
-          <p> &#x2022; Software Evolution & Maintenance</p>
-        </div>
+         
 
         <div className="grid grid-cols-1 justify-center items-center md:grid-cols-4 ">
           <div className="flex justify-center items-center mt-2">
@@ -58,15 +54,9 @@ function Education() {
           </div>
         </div>
 
-        <div className="flex flex-col justify-center items-center  my-2">
-          <h1 className="text-2xl font-bold">Additional Courses </h1>
-          <p> &#x2022; Cryptography and Network Security</p>
-          <p> &#x2022; Ethical Hacking &Penetration Testing</p>
-          <p> &#x2022; Information Systems Audit & Assurance</p>
-          <p> &#x2022; Fundamentals for Data Science</p>
-          <p> &#x2022; Data Visualization and Communication</p>
-          <p> &#x2022; Software Project Management</p>
-        </div>
+    
+
+
       </div>
     </div>
   );
